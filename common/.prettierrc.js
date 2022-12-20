cfc96e1c338d0cf15470437fc75a404d229a7a24
苦力怕๑ <103903530@qq.com>
2022-12-20 16:11:29 +0800

    some file

---
+++ common/.prettierrc.js
@@ -0,0 +1,17 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "consistent",
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: false,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  singleAttributePerLine: false,
+};
